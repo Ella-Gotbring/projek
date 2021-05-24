f6cdf297c621a4df994b8d674f937026024b1e27
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2021-05-24 11:48:20 +0200

    users

---
+++ frontend/src/views/Feed.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <router-link :to="{ name:'Signup' }">
+      <img alt="Vue logo" src="../assets/logo.png">
+
+    </router-link>
+
+   
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Feed',
+  
+}
+</script>
---
+++ frontend/src/views/Settings.vue
@@ -0,0 +1,14 @@
+<template>
+<div></div>
+    
+</template>
+
+<script>
+export default {
+    name: "Settings"
+}
+</script>
+
+<style scoped>
+
+</style>
