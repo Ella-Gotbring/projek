35004362b8076dfe50e2d3ce899ab8a204b1eea9
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2021-05-26 11:15:52 +0200

    added settings view to add streams

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,11 +1,49 @@
 <template>
-<div></div>
+<div>
+    <div class="list">
+      <ul>
+        <li class="list-item" v-for="stream in streams" :key="stream._id">
+          <p>{{ stream.tags }}</p>
+        </li>
+      </ul>
+    </div>
+    <button @click="deleteMe">Shit they're on to mee</button>
+</div>
     
 </template>
 
 <script>
+import axios from "axios";
 export default {
-    name: "Settings"
+  name: "Settings",
+  async created() {
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${sessionStorage.getItem("auth")}`;
+    const res = await axios.get("http://localhost:5000/api/flow");
+    this.streams = res.data;
+    console.log(res.data);
+  },
+  data() {
+    return {
+      userID: null,
+      streams: [],
+    };
+  },
+  methods:{
+      deleteMe(){
+        return axios
+        .delete("/api/user", {
+          headers: {
+            "Content-type": "application/json",
+          },
+        })
+        .then(() => {
+          sessionStorage.removeItem("auth");
+          this.$router.push({ name: "Login" });
+        });
+    },
+  },
 }
 </script>
 
